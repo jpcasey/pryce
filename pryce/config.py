6ce28758aa09347fced4bd66bfa923c2330f8fdb
Casey Boettcher <chb@socketbox.com>
2020-01-30 13:58:45 -0500

    Revert "Adding GCP config using datastore"
    This reverts commit 2e92b102fa081a9282fb606c27ac0a60577218b6.

--- pryce/config.py
+++ pryce/config.py
@@ -1,25 +1,6 @@
 import os
-import logging
 
 class Config(object):
-    logger = logging.getLogger()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    if os.getenv('GOOGLE_CLOUD_PROJECT'):
-        from google.cloud import datastore
-        client = datastore.Client()
-        query = client.query(kind='database')
-        result = list(query.fetch())
-        #assume that only one entity in 'database' kind; so result size is one         
-        csql_instance = result[0]['csql_instance']
-        csql_database = result[0]['csql_database']
-        csql_user = result[0]['csql_user'] 
-        csql_pass = result[0]['csql_pass']
-        csql_connection_string =\
-            'postgres+pg8000://{}:{}@/{}?unix_sock=/cloudsql/{}/.s.PGSQL.5432'.format(\
-            #'postgres+psycopg2://{}:{}@/{}?unix_sock=/cloudsql/{}/.s.PGSQL.5432'.format(\
-            csql_user, csql_pass, csql_database, csql_instance)
-        logger.log("csql_connection_string: " + csql_connection_string)
-        SQLALCHEMY_DATABASE_URI = csql_connection_string
-    else:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 

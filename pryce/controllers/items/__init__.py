c53d71ee1b245dd3caab7412083fd1894ac13f50
John Casey III <jpcasey07@gmail.com>
2020-02-02 22:56:22 -0500

    initial implementation of items controller and updated marshmallow schemas

--- pryce/controllers/items/__init__.py
+++ pryce/controllers/items/__init__.py
@@ -1,74 +1,79 @@
 from flask import Blueprint, request, jsonify
+from pryce.database.models import Item, Price
+from pryce.database.schemas import ItemSchema, PriceSchema
+from pryce import db
 
 bp = Blueprint('items', __name__, url_prefix='/items')
-
-# mocks for testing some of the routes
-mock_items = [
-    { 
-        'id': '1',
-        'name': 'item1',
-        'code': '1823924829'
-    },
-    { 
-        'id': '2',
-        'name': 'item2',
-        'code': '923481239'
-    },
-]
+item_schema = ItemSchema()
 
 # / - GET
 # Returns a list of items in the system.  Can be filtered using query parameters (e.g. name, brand)
 @bp.route('/', methods=['GET'])
 def get_items():
-    return jsonify({'items': mock_items})
+    items = Item.query.all()
+    return item_schema.jsonify(items, many=True)
 
 # /- POST
 # Adds an item to the system.
 @bp.route('/', methods=['POST'])
 def add_item():
-    content = request.get_json()
-    print(content)
-    name = content['name']
-    barcode= content['barcode']
-    message = f'Added item {name}, {barcode}'
-    return jsonify(message)
+    req_body = request.get_json()
+    if 'name' not in req_body or 'code' not in req_body:
+        return jsonify(message='Name and brand are required attributes.'), 400
+    name = req_body.get('name')
+    code = req_body.get('code')
+    brand = req_body.get('brand', None)
+    weight = req_body.get('weight', 0)
+    description = req_body.get('description', '')
+    item = Item(name=name, brand=brand, code=code, weight=weight, description=description)
+    db.session.add(item)
+    db.session.commit()
+    return item_schema.jsonify(item)
 
 # /<item_id> - GET
 # Returns information for a specific item.
 @bp.route('/<item_id>', methods=['GET'])
 def get_item(item_id):
-    item = None
-    for mock_item in mock_items:
-        print(mock_item)
-        print(item_id)
-        if mock_item['id'] == item_id:
-            item = mock_item
-    
-    if item != None:
-        return jsonify({'message': 'Here\'s your item!', 'item': item})
-    else:
-        return jsonify({'message': 'Item not found'}), 404
+    item = Item.query.filter_by(item_id=item_id).first()
+    if item == None:
+        return jsonify(message='Item not found'), 404
+    return item_schema.jsonify(item)
 
 # /<item_id> - PUT
 # Update information for a specific item.
 @bp.route('/<item_id>', methods=['PUT'])
 def update_item(item_id):
-    item = {'name': 'item' + item_id, 'price': 8.00},
-
-    return jsonify({'message': 'Here\'s your item!', 'item': item})
+    item = Item.query.filter_by(item_id=item_id).first()
+    if item == None:
+        return jsonify(message = 'Item not found'), 404
+    req_body = request.get_json()
+    item.name = req_body.get('name', item.name)
+    item.brand = req_body.get('brand', item.brand)
+    item.weight = req_body.get('weight', item.weight)
+    item.description = req_body.get('description', item.description)
+    db.session.commit()
+    return item_schema.jsonify(item)
 
 # /<item_id> - DELETE
 # Deletes an item from the system.
 @bp.route('/<item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    item = {'name': 'item' + item_id, 'price': 8.00},
-
-    return jsonify({'message': 'Here\'s your item!', 'item': item})
+    item = Item.query.filter_by(item_id=item_id).first()
+    if item != None:
+        db.session.delete(item)
+        db.session.commit()
+        message = f'Successfully deleted "{item.name}"'
+        return jsonify(message = message)
+    return jsonify(message = 'Item not found'), 404
 
 # /<item_id>/prices - GET
-# Returns store & price information for a specific item.  Can be filtered using query parameters (e.g. store, location, price, date last verified).
+# Returns store & price information for a specific item.
+# todo: handle appropriate query parameters (e.g. store, location, price, date last verified).
+# todo: currently returns all prices. should group prices by store and only show the most recent for each store
 @bp.route('/<item_id>/prices', methods=['GET'])
 def get_item_prices(item_id):
-    item = {'name': 'item' + item_id, 'price': 8.00},
-
-    return jsonify({'message': 'Here\'s your item!', 'item': item})
+    prices = Price.query.filter_by(item_id=item_id).order_by(Price.reported.desc()).all()
+    if prices == None:
+        return jsonify(message = 'Item not found.'), 404
+    price_schema = PriceSchema()
+    return price_schema.jsonify(prices, many=True)
--- pryce/database/schemas.py
+++ pryce/database/schemas.py
@@ -1,5 +1,6 @@
 # Schemas for Flask-Marshmallow to easily JSON serialize SQLAlchemy resultsets
 from pryce import ma
+from marshmallow import fields
 from pryce.database.models import *
 import simplejson
 
@@ -7,48 +8,16 @@ class ItemSchema(ma.ModelSchema):
     class Meta:
         json_module = simplejson
         model = Item
+        exclude = ["prices", "list_items"]
         
-class PriceSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = Price
-
 class StoreSchema(ma.ModelSchema):
     class Meta:
         json_module = simplejson
         model = Store
 
-class AppuserSchema(ma.ModelSchema):
+class PriceSchema(ma.ModelSchema):
     class Meta:
         json_module = simplejson
-        model = Appuser
-
-class BadgeSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = Badge
-
-class CommentSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = Comment
-
-class ImageSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = Image
-
-class ListSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = List
-
-class ListItemSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = ListItem
-
-class LocationSchema(ma.ModelSchema):
-    class Meta:
-        json_module = simplejson
-        model = Location
+        model = Price
+    item = fields.Nested(ItemSchema(only=("item_id", "brand", "name", "code")))
+    store = fields.Nested(StoreSchema(only=("store_id", "name")))

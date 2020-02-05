a053c727957be275e8ce0079765b2d1f2c4b8026
Casey Boettcher <chb@socketbox.com>
2020-02-05 18:01:51 -0500

    Drop the image table and add image attributes to Models

--- pryce/database/models.py
+++ pryce/database/models.py
@@ -1,18 +1,10 @@
-# coding: utf-8
-from sqlalchemy import BigInteger, CheckConstraint, Column, DateTime, Float, ForeignKey, Integer, Numeric, String, Table, Text
-from sqlalchemy.orm import relationship
-from sqlalchemy.schema import FetchedValue
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-from .. import db
+from pryce.database.dal import db
 
 
 class Acces(db.Model):
     __tablename__ = 'access'
 
     access_id = db.Column(db.Integer, primary_key=True)
-
 
 
 class Appuser(db.Model):
@@ -23,12 +15,11 @@ class Appuser(db.Model):
     password = db.Column(db.String)
     home = db.Column(db.ForeignKey('location.location_id', ondelete='RESTRICT', onupdate='CASCADE'))
     karma = db.Column(db.Integer)
-    avatar = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
-
-    image = db.relationship('Image', primaryjoin='Appuser.avatar == Image.image_id', backref='appusers')
+    #avatar = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
+    #image = db.relationship('Image', primaryjoin='Appuser.avatar == Image.image_id', backref='appusers')
+    image = db.Column(db.String)
     location = db.relationship('Location', primaryjoin='Appuser.home == Location.location_id', backref='appusers')
     badges = db.relationship('Badge', secondary='badge_appuser', backref='appusers')
-
 
 
 class Badge(db.Model):
@@ -36,23 +27,22 @@ class Badge(db.Model):
 
     badge_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    image_id = db.Column(db.BigInteger)
-
+    image = db.Column(db.String)
 
 
 t_badge_appuser = db.Table(
     'badge_appuser',
-    db.Column('badge_id', db.ForeignKey('badge.badge_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False),
-    db.Column('appuser_id', db.ForeignKey('appuser.appuser_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False)
+    db.Column('badge_id', db.ForeignKey('badge.badge_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True,
+              nullable=False),
+    db.Column('appuser_id', db.ForeignKey('appuser.appuser_id', ondelete='RESTRICT', onupdate='CASCADE'),
+              primary_key=True, nullable=False)
 )
-
 
 
 class Chain(db.Model):
     __tablename__ = 'chain'
 
     chain_id = db.Column(db.Integer, primary_key=True)
-
 
 
 class Comment(db.Model):
@@ -69,18 +59,16 @@ class Comment(db.Model):
 
     appuser = db.relationship('Appuser', primaryjoin='Comment.appuser_id == Appuser.appuser_id', backref='comments')
 
-
-
+'''
 class Image(db.Model):
     __tablename__ = 'image'
 
     image_id = db.Column(db.Integer, primary_key=True)
-    fspath = db.Column(db.String, unique=True)
+    uri = db.Column(db.String, unique=True)
     imgtype = db.Column(db.String)
     width = db.Column(db.Integer)
     height = db.Column(db.Integer)
-
-
+'''
 
 class Item(db.Model):
     __tablename__ = 'item'
@@ -89,13 +77,12 @@ class Item(db.Model):
     code = db.Column(db.String, nullable=False, unique=True)
     name = db.Column(db.String, nullable=False)
     brand = db.Column(db.String)
-    weight = db.Column(db.Numeric, nullable=True)
-    weight_unit = db.Column(db.String, server_default=db.FetchedValue())
-    image_id = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
+    quantity = db.Column(db.Numeric, nullable=True)
+    quant_unit = db.Column(db.String, server_default=db.FetchedValue())
+    #image_id = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
     description = db.Column(db.Text, nullable=False)
-
-    image = db.relationship('Image', primaryjoin='Item.image_id == Image.image_id', backref='items')
-
+    image = db.Column(db.String)
+    #image = db.relationship('Image', primaryjoin='Item.image_id == Image.image_id', backref='items')
 
 
 class List(db.Model):
@@ -104,23 +91,23 @@ class List(db.Model):
     list_id = db.Column(db.Integer, primary_key=True)
     owner = db.Column(db.ForeignKey('appuser.appuser_id', ondelete='RESTRICT', onupdate='CASCADE'))
     access_id = db.Column(db.ForeignKey('access.access_id', ondelete='RESTRICT', onupdate='CASCADE'))
-
     access = db.relationship('Acces', primaryjoin='List.access_id == Acces.access_id', backref='lists')
     appuser = db.relationship('Appuser', primaryjoin='List.owner == Appuser.appuser_id', backref='lists')
-
 
 
 class ListItem(db.Model):
     __tablename__ = 'list_item'
 
-    item_id = db.Column(db.ForeignKey('item.item_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False)
-    list_id = db.Column(db.ForeignKey('list.list_id'), db.ForeignKey('list.list_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True, nullable=False)
+    item_id = db.Column(db.ForeignKey('item.item_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True,
+                        nullable=False)
+    list_id = db.Column(db.ForeignKey('list.list_id'),
+                        db.ForeignKey('list.list_id', ondelete='RESTRICT', onupdate='CASCADE'), primary_key=True,
+                        nullable=False)
     quantity = db.Column(db.Integer, server_default=db.FetchedValue())
 
     item = db.relationship('Item', primaryjoin='ListItem.item_id == Item.item_id', backref='list_items')
     list = db.relationship('List', primaryjoin='ListItem.list_id == List.list_id', backref='list_list_items')
     list1 = db.relationship('List', primaryjoin='ListItem.list_id == List.list_id', backref='list_list_items_0')
-
 
 
 class Location(db.Model):
@@ -131,7 +118,6 @@ class Location(db.Model):
     long = db.Column(db.Float)
 
 
-
 class Price(db.Model):
     __tablename__ = 'price'
 
@@ -139,7 +125,7 @@ class Price(db.Model):
     currency = db.Column(db.String(3))
     item_id = db.Column(db.ForeignKey('item.item_id', ondelete='RESTRICT', onupdate='CASCADE'))
     appuser_id = db.Column(db.ForeignKey('appuser.appuser_id', ondelete='RESTRICT', onupdate='CASCADE'))
-    price = db.Column(db.Numeric(12,3))
+    price = db.Column(db.Numeric(12, 3))
     reported = db.Column(db.DateTime(True), nullable=False)
     store_id = db.Column(db.ForeignKey('store.store_id', ondelete='RESTRICT', onupdate='CASCADE'))
 
@@ -148,16 +134,15 @@ class Price(db.Model):
     store = db.relationship('Store', primaryjoin='Price.store_id == Store.store_id', backref='prices')
 
 
-
 class Store(db.Model):
     __tablename__ = 'store'
-
     store_id = db.Column(db.Integer, primary_key=True)
     location_id = db.Column(db.ForeignKey('location.location_id', ondelete='RESTRICT', onupdate='CASCADE'))
     chain_id = db.Column(db.ForeignKey('chain.chain_id', ondelete='RESTRICT', onupdate='CASCADE'))
     name = db.Column(db.String)
-    image_id = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
-
+    #image_id = db.Column(db.ForeignKey('image.image_id', ondelete='RESTRICT', onupdate='CASCADE'))
+    image = db.Column(db.String)
     chain = db.relationship('Chain', primaryjoin='Store.chain_id == Chain.chain_id', backref='stores')
-    image = db.relationship('Image', primaryjoin='Store.image_id == Image.image_id', backref='stores')
+    #image = db.relationship('Image', primaryjoin='Store.image_id == Image.image_id', backref='stores')
     location = db.relationship('Location', primaryjoin='Store.location_id == Location.location_id', backref='stores')
+

d430271b8ef54cdee1aa428aacb4348faa494ea5
Casey Boettcher <chb@socketbox.com>
2020-02-10 08:19:01 -0500

    Fixed error in dal/item.py that referred to store.

--- pryce/database/dal/item.py
+++ pryce/database/dal/item.py
@@ -29,7 +29,7 @@ class DALItem:
             db.session.commit()
         return item
 
-    def delete_item(self, store):
-        rows = Item.query.filter_by(store_id=store.store_id).delete()
+    def delete_item(self, item_id):
+        rows = Item.query.filter_by(item_id=item_id).delete()
         db.session.commit()
         return rows

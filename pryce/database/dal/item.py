24f2e0f53f7d47519dc5e07f4ddd4b763841d8a4
Casey Boettcher <chb@socketbox.com>
2020-03-08 16:25:47 -0400

    Removing workaround for mock data; lowest price returned for list details

--- pryce/database/dal/item.py
+++ pryce/database/dal/item.py
@@ -40,11 +40,3 @@ class DALItem:
         db.session.commit()
         return rows
 
-    def get_search_list_items(self):
-        plain_sql = """with table1 as (select row_number() over (partition by pri.item_id order by pri.reported desc) as rn,
-                    pri.price, pri.item_id, pri.store_id, pri.reported from price pri ) select itm.name as item_name, itm.code, table1.item_id, table1.reported, table1.price, sto.place_id, sto.name as store_name
-                  from item itm inner join table1 on table1.item_id = itm.item_id inner join store sto on table1.store_id = sto.store_id where rn=1;"""
-        sql = text(plain_sql)
-        result = db.engine.execute(sql)
-        return result
-
